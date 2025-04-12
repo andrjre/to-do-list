bc244cbf634cb2dbdfaaf53e02eb4157a0dc3671
andrjre <andrjre@gmail.com>
2025-04-12 17:33:06 +0100

    Troubleshooting.

--- index.js
+++ index.js
@@ -3,30 +3,46 @@ let create = document.getElementById("create");
 let popup = document.getElementById("popup");
 let submit = document.getElementById("submit");
 let body = document.body;
-let type = document.getElementById("type").value;
-let date = document.getElementById("date").value;
-let time = document.getElementById("time").value;
+let type
+let date
+let time
 popup.style.display = "none";
 
 let darkMode = document.getElementById("darkMode")
 let lightMode = document.getElementById("darkMode")
-let modeDecide = 0
 
 create.onclick = function(){
-   
     popup.style.display = "";
-    console.log(type)
-
-    if(type.value == undefined){
-        submit.style.background = "red"
-    }
-    
 }
 
+
+if(document.getElementById("type").value !== "0"){
+    console.log("nooo")
+    document.getElementById("submit").disabled = false;
+    document.getElementById("submit").style.backgroundColor = "green"
+}
+if(document.getElementById("type").value == "0"){
+    console.log("gello")
+    document.getElementById("submit").disabled = true;
+    document.getElementById("submit").style.backgroundColor = "red"
+}
+//somethign weird with this bit 
+
+
 submit.onclick = function(){
-    
+
     popup.style.display = "none"
-   
+    type = document.getElementById("type").value;
+    date = document.getElementById("date").value;
+    time = document.getElementById("time").value;
+
+    
+
+    console.log(type)
+    console.log(date)
+    console.log(time)
+
+    document.createElement("div")
 }
 
 darkMode.onclick = function(){
@@ -35,5 +51,5 @@ darkMode.onclick = function(){
     darkMode.style.color = "white"
     body.style.backgroundColor = "black"
     body.style.color = "white"
-    modeDecide = 1
 }
+
